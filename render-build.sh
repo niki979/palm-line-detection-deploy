380328ee13a7d5853ce302049f579cb1fb5e4188
Niki Patel <67195763+niki979@users.noreply.github.com>
2024-09-28 22:59:43 +0530

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
 git lfs install
+
 git lfs pull
+
 pip install -r requirements.txt
