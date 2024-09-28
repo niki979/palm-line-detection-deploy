26917751473b7820097e65ce708335a27e24546c
Niki Patel <67195763+niki979@users.noreply.github.com>
2024-09-28 23:53:44 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
 pip install gdown
 
-gdown --id vk0pitwQDA7WS2rkjMxNPuYDShh1JLpv --output model2.h5
+gdown --id 1vk0pitwQDA7WS2rkjMxNPuYDShh1JLpv --output model2.h5
 
 pip install -r requirements.txt
